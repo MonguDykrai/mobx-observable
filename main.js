148116d2576053210cdeb9689c2431c72975c14d
MonguDykrai <40307989+MonguDykrai@users.noreply.github.com>
2023-04-25 13:11:07 +0800

    computed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { observable, autorun, action } from 'mobx';
+import { observable, autorun, action, computed } from 'mobx';
 
 let cart = observable({
   itemCount: 0,
@@ -9,6 +9,15 @@ autorun(() => {
   console.log(`The Cart contains ${cart.itemCount} item(s).`);
 }); // => 控制台输出： The Cart containers 0 item(s)
 
+// 不管用，不知道为什么
+// const computedCartItemCount = computed(() => {
+//   debugger;
+//   console.log('cart.itemCount', cart.itemCount);
+//   return `itemCount: ${cart.itemCount}`;
+// });
+
+// console.log(computedCartItemCount);
+
 const app = document.getElementById('app');
 
 const button = document.createElement('button');
@@ -16,7 +25,7 @@ button.textContent = 'itemCount + 1';
 button.addEventListener(
   'click',
   action((e) => {
-    console.log(e);
+    // console.log(e);
     cart.itemCount = cart.itemCount + 1;
   })
 );
